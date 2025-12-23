54aadc013906c4d1e7b93620a0e5dffe166e1597
imad <imad@imad.com>
2025-12-23 21:28:53 +0100

    4

---
+++ assets/about-us-Ch6uEFKX.js
@@ -0,0 +1,1 @@
+import{A as o}from"./about-us-DwIGkb4n.js";import"./index-Ta-mg3OE.js";import"./card-BD-VpUh2.js";import"./static-ChLg62l2.js";const i=o;export{i as component};
---
+++ assets/index-BSSoYlcE.js
@@ -0,0 +1,1 @@
+import{l as o,A as e}from"./index-BcWs6MQv.js";const r=function(){const a=o(),t=localStorage.getItem("lang");if(t){a({to:"/$lang",params:{lang:t}});return}else{const n=e.get("lang")??"ar";a({to:"/$lang",params:{lang:n}}),localStorage.setItem("lang",n)}};export{r as component};
